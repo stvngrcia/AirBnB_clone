61ea506ad4c5fb91f6a2bc490b422c860d8da41d
stvngrcia <stevengarciaacuna@gmail.com>
2018-02-09 11:14:43 -0800

    fixed to_dict call, it was just returning the method name and not the actual dictionary

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,7 @@ class FileStorage:
     '''
     __file_path = "file.json"
     __objects = {}
-    
+
     def all(self):
         '''
             return the dictionary
@@ -22,8 +22,8 @@ class FileStorage:
         '''
             set in __objects the obj with key <obj class name>.id
         '''
-        key = str(obj.__class__.__name__) + str(obj.id)
-        value_dict = obj.to_dict
+        key = str(obj.__class__.__name__) + "." + str(obj.id)
+        value_dict = obj.to_dict()
         FileStorage.__objects[key] = value_dict
 
     def save(self):
@@ -31,7 +31,7 @@ class FileStorage:
             serializes __objects attribute to JSON file.
         '''
         with open(FileStorage.__file_path, mode='w', encoding="UTF8") as fd:
-           json.dump(FileStorage.__objects,fd) 
+           json.dump(FileStorage.__objects,fd)
 
     def reload(self):
         '''
