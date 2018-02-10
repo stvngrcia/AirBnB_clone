7753d6c08699bb278c5eca9c8d699b947f394b89
rayraib <binitarai11@gmail.com>
2018-02-10 18:59:04 +0000

    implement show method that prints the string representation of an instance based on the class name and id given as args

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
     Implementing the console for the HBnB project.
 '''
 import cmd
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 
 
@@ -38,9 +39,23 @@ class HBNDCommand(cmd.Cmd):
             args = args.split(" ")
             new_instance = eval(args[0])()
             new_instance.save()
+            print(new_instance.id)
 
         except:
             print("** class doesn't exist **")
+
+    def do_show(self, args):
+        '''
+            Print the string representation of an instance baed on 
+            the class name and id given as args.
+        '''
+        args = args.split(" ")
+        storage = FileStorage()
+        storage.reload()
+        obj_dict = storage.all()
+        key = args[0] + "." + args[1]  
+        value = obj_dict[key]
+        print(value)
 
 if __name__ == "__main__":
     HBNDCommand().cmdloop()
