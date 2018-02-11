f2307cf505386c7b43a32d522e496f9465ff7e30
rayraib <binitarai11@gmail.com>
2018-02-11 18:34:18 +0000

    create class City with attribute state_id and name

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+    Define the class City.
+'''
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    '''
+        Define the class City that inherits from BaseModel.
+    '''
+    state_id = ""
+    name = ""
