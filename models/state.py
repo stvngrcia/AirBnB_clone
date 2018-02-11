5894bd15a18d563daa856d3885d3ce72acc99c80
stvngrcia <stevengarciaacuna@gmail.com>
2018-02-11 10:29:13 -0800

    adding a class for the states

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+    Implementation of the State class
+'''
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    '''
+        Implementation for the State.
+    '''
+    name = ""
