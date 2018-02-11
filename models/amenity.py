6f90a0a6eb6b46e4b4293724de696b5eeb4dacf7
stvngrcia <stevengarciaacuna@gmail.com>
2018-02-11 10:28:15 -0800

    adding a class for amenities

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+    Implementation of the Amenity class
+'''
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    '''
+        Implementation for the Amenities.
+    '''
+    name = ""
