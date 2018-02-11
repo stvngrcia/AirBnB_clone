eedd34b44ee9deb7f54af894c4ecaff1e13248a1
rayraib <binitarai11@gmail.com>
2018-02-11 18:53:12 +0000

    import Place, State, City, Amenity and Review class from model

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -44,6 +44,11 @@ class FileStorage:
         '''
         from models.base_model import BaseModel
         from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
 
         try:
             with open(FileStorage.__file_path, encoding="UTF8") as fd:
