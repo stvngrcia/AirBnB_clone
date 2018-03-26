4f5447dea9d844b5609e959559f0db4d8935a0e5
Yunju Chen <info@yunjuchen.net>
2018-03-26 06:14:22 +0000

    Add doc string

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -13,6 +13,7 @@ from models.state import State
 from models.user import User
 from os import os
 
+
 class DBStorage:
     '''
     Class DBStorage for database in MySQL
@@ -24,6 +25,9 @@ class DBStorage:
     __session = None
 
     def __init__(self):
+        '''
+        Instantiate DBStorage
+        '''
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
                                       .format(HBNB_MYSQL_USER, HBNB_MYSQL_PWD,
                                               HBNB_MYSQL_HOST, HBNB_MYSQL_DB),
@@ -38,16 +42,29 @@ class DBStorage:
         if cls is not None:
             for obj in session.query(cls):
                 print(obj)
+
     def new(self, obj):
+        '''
+        Add a new object to database
+        '''
         self.__session.add(self)
 
     def save(self):
+        '''
+        Save change to database
+        '''
         self.__session.commit(self)
 
     def delete(self, obj=None):
+        '''
+        Delete an object from database
+        '''
         self.__session.delete(self)
 
     def reload(self):
+        '''
+        To reload data from database
+        '''
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
