6e58401670965ca55d4a80069fc4357792fca072
jagrvargen <jagarthewolf@gmail.com>
2018-03-27 19:50:12 +0000

    updated user, place, and city to create relationship

--- models/user.py
+++ models/user.py
@@ -17,3 +17,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+    places = relationship('Place', cascade='all, delete', backref='user')
