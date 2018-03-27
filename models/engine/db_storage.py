93231745702b87ecd665db8c2e03b0a8c4ed97bb
Yunju Chen <info@yunjuchen.net>
2018-03-27 00:36:11 +0000

    Add all()

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -19,6 +19,7 @@ HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
 HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
 HBNB_ENV = getenv('HBNB_ENV')
 
+
 class DBStorage:
     '''
     Class DBStorage for database in MySQL
@@ -37,36 +38,44 @@ class DBStorage:
                                       .format(HBNB_MYSQL_USER, HBNB_MYSQL_PWD,
                                               HBNB_MYSQL_HOST, HBNB_MYSQL_DB),
                                       pool_pre_ping=True)
-        if getenv('HBNB_ENV') == 'test':
+        if HBNB_ENV == 'test':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         '''
         Method that creates a query and prints a dictionary of cls
         '''
+        obj_dict = {}
         if cls is not None:
             for obj in session.query(cls):
-                print(obj)
-#                print("[{}] ({}) {}".format(cls, obj.id, )
+                key = cls + '.' + obj.id
+                obj_dict[key] = obj
+        else
+            all_tables = metadata.tables.keys()
+            for cls in all_tables:
+                for obj in session.query(cls):
+                    key = cls + '.' + obj.id
+                    obj_dict[key] = obj
+        return obj_dict
 
     def new(self, obj):
         '''
         Add a new object to database
         '''
-        self.__session.add(self)
+        self.__session.add(obj)
 
     def save(self):
         '''
         Save change to database
         '''
-        self.__session.commit(self)
+        self.__session.commit()
 
     def delete(self, obj=None):
         '''
         Delete an object from database if not none
         '''
         if obj is not None:
-            self.__session.delete(self)
+            self.__session.delete(obj)
 
     def reload(self):
         '''
@@ -75,4 +84,4 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
-        self.__sessioin = scoped_session(session_factory)
+        self.__session = scoped_session(session_factory)
