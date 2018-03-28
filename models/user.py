272e41c280bbecf3a0721a98237a76118bbf8f0c
jagrvargen <jagarthewolf@gmail.com>
2018-03-28 04:11:36 +0000

    changed name defaults to nullable

--- models/user.py
+++ models/user.py
@@ -15,6 +15,6 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
     places = relationship('Place', cascade='all, delete', backref='user')
