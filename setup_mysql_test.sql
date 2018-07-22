3829d68d2ac110414894242ddf2bd315b2fae3dc
vmdn23 <victormdnguyen88@gmail.com>
2018-07-22 14:02:15 -0700

    add sql server setup

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- Prepares a MySQL server for the projact
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
+GRANT USEAGE ON *.* TO 'hbnb_test@localhost'
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test@localhost'
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test@localhost'
