4200422a7ccb61edd85c25abddc69e8781be0972
vmdn23 <victormdnguyen88@gmail.com>
2018-07-22 22:39:46 +0000

    add a delete function for file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,7 +13,7 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         '''
             Return the dictionary
         '''
@@ -53,3 +53,12 @@ class FileStorage:
                 FileStorage.__objects[key] = class_name(**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        '''
+        Test delete feature
+        '''
+        if obj is not None:
+            key = str(obj.__class__.__name__) + "." + str(obj.id)
+            FileStorage().__objects.pop(key, None)
+            FileStorage().save()
