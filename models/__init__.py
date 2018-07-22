dc0ed3f1e5c1424b9ed7ec2efa5ac16868a311f1
gjdame <gregoryjdame@gmail.com>
2018-07-22 21:31:13 +0000

    add db storage first attempt

--- models/__init__.py
+++ models/__init__.py
@@ -2,8 +2,7 @@
 '''
     Package initializer
 '''
-
-from models.engine.file_storage import FileStorage
+import os
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
@@ -12,10 +11,18 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
-classes = {"User": User, "BaseModel": BaseModel,
-           "Place": Place, "State": State,
-           "City": City, "Amenity": Amenity,
-           "Review": Review}
+storage_type = os.getenv('HBNB_TYPE_STORAGE')
 
-storage = FileStorage()
-storage.reload()
+if storage_type == db:
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    classes = {"User": User, "BaseModel": BaseModel,
+               "Place": Place, "State": State,
+               "City": City, "Amenity": Amenity,
+               "Review": Review}
+
+    storage = FileStorage()
+    storage.reload()
