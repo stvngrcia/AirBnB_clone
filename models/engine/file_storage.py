1badecc3a91ed367b2ce6a3109cce5ffe99923ab
AmyLGalles <suzyhendrix@gmail.com>
2018-07-22 01:41:53 +0000

    added prototype changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,7 +13,7 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         '''
             Return the dictionary
         '''
@@ -53,3 +53,9 @@ class FileStorage:
                 FileStorage.__objects[key] = class_name(**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        '''
+            Deletes an object from database
+        '''
+        pass
