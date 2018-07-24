7a368a0154634fc7ec05ee7dd4bcbc87977bd4c9
vmdn23 <victormdnguyen88@gmail.com>
2018-07-24 15:15:43 +0000

    update review for db, not done

--- models/review.py
+++ models/review.py
@@ -3,13 +3,24 @@
     Implementation of the Review class
 '''
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Review(BaseModel):
     '''
         Implementation for the Review.
     '''
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = "reviews"
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False))
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False))
+
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
