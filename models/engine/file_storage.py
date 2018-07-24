a65567fc56a349db8b3fd8357f5b49d148ea710c
Isaac Wong <isaac@thirdcaptain.com>
2018-07-24 00:20:53 +0000

    Test copies

---
+++ models/engine/file_storage.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+'''
+    Define class FileStorage
+'''
+import json
+import models
+
+
+class FileStorage:
+    '''
+        Serializes instances to JSON file and deserializes to JSON file.
+    '''
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self, cls=None):
+        '''
+            Return the dictionary
+        '''
+        return self.__objects
+
+    def new(self, obj):
+        '''
+            Set in __objects the obj with key <obj class name>.id
+            Aguments:
+                obj : An instance object.
+        '''
+        key = str(obj.__class__.__name__) + "." + str(obj.id)
+        value_dict = obj
+        FileStorage.__objects[key] = value_dict
+
+    def save(self):
+        '''
+            Serializes __objects attribute to JSON file.
+        '''
+        objects_dict = {}
+        for key, val in FileStorage.__objects.items():
+            objects_dict[key] = val.to_dict()
+
+        with open(FileStorage.__file_path, mode='w', encoding="UTF8") as fd:
+            json.dump(objects_dict, fd)
+
+    def reload(self):
+        '''
+            Deserializes the JSON file to __objects.
+        '''
+        try:
+            with open(FileStorage.__file_path, encoding="UTF8") as fd:
+                FileStorage.__objects = json.load(fd)
+            for key, val in FileStorage.__objects.items():
+                class_name = val["__class__"]
+                class_name = models.classes[class_name]
+                FileStorage.__objects[key] = class_name(**val)
+        except FileNotFoundError:
+            pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects if it’s inside
+        Args:
+            obj: input object class
+        """
+        new_dict = dict(FileStorage.__objects)
+        key_del = obj
+        for key, value in new_dict.items():
+            if value == key_del:
+                del(obj)
+                del FileStorage.__objects[key]
+                self.save()
