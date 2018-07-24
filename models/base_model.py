e8f0e2be2a2d6c20894612aa8c124ccc55186219
Isaac Wong <isaac@thirdcaptain.com>
2018-07-24 20:54:51 +0000

    Updated __init__

--- models/base_model.py
+++ models/base_model.py
@@ -27,18 +27,27 @@ class BaseModel:
         '''
             Initialize public instance attributes.
         '''
-        for key, val in kwargs.items():
-            if "__class__" not in key:
-                setattr(self, key, val)
-        if "id" not in kwargs:
+        if (len(kwargs) == 0):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-        else:
+            models.storage.new(self)
+        if "created_at" in kwargs:
             kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
                                                      "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.created_at = datetime.now()
+        if "updated_at" in kwargs:
             kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
                                                      "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.updated_at = datetime.now()
+
+        if "id" not in kwargs:
+            self.id = str(uuid.uuid4())
+        for key, val in kwargs.items():
+            if "__class__" not in key:
+                setattr(self, key, val)
 
     def __str__(self):
         '''
