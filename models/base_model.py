b94e1554709940e9305b4298cd0ee694b3a1c954
Kevin Yook <yook00627@gmail.com>
2018-07-25 12:13:56 -0700

    fix issue with id

--- models/base_model.py
+++ models/base_model.py
@@ -43,6 +43,9 @@ class BaseModel:
         else:
             self.updated_at = datetime.now()
 
+        if "id" not in kwargs:
+            self.id = str(uuid.uuid4())
+
         for key, val in kwargs.items():
             if "__class__" not in key:
                 setattr(self, key, val)
