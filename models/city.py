4c580cb088b9dbe5ebeb393c287a42a88841502a
Lisa Olson <olson.lisa94@gmail.com>
2018-07-25 22:23:02 +0000

    adds check for db

--- models/city.py
+++ models/city.py
@@ -2,6 +2,8 @@
 '''
     Define the class City.
 '''
+
+from os import getenv
 import sqlalchemy
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, create_engine, DateTime
@@ -10,19 +12,19 @@ from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import ForeignKey
 
+storage_type = getenv('HBNB_TYPE_STORAGE')
 
 class City(BaseModel, Base):
     '''
         Define the class City that inherits from BaseModel.
     '''
     __tablename__ = 'cities'
-    name = Column(
-        String(128),
-        nullable=False
-    )
-    state_id = Column(
-        String(60),
-        ForeignKey("states.id"),
-        nullable=False
-    )
-    places = relationship("Place", cascade="all, delete-orphan", backref="cities")
+    if (storage_type == 'db'):
+        name = Column(
+            String(128),
+            nullable=False
+        )
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship("Place", cascade="all, delete-orphan", backref="cities")
+    else:
+        name = ""
