100e7fe18baa13bc3656f96cfd8a76d5fd5ce5d1
Lisa Olson <olson.lisa94@gmail.com>
2018-07-26 03:51:37 +0000

    grabs environ variable in conditional

--- models/user.py
+++ models/user.py
@@ -9,18 +9,19 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
 
-storage_type = getenv('HBNB_TYPE_STORAGE')
 
 class User(BaseModel, Base):
     '''
         Definition of the User class
     '''
     __tablename__ = 'users'
-    if (storage_type == 'db'):
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        reviews = relationship("Review", cascade="all, delete-orphan")
+        places = relationship("Place", cascade="all, delete-orphan")
 
     else:
         email = ""
