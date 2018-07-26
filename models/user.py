2b0fdc9aa4bb69dd8e89e16e406c62afdfbf7c93
Lisa Olson <olson.lisa94@gmail.com>
2018-07-26 03:33:50 +0000

    places class attributes back under if statement

--- models/user.py
+++ models/user.py
@@ -16,12 +16,11 @@ class User(BaseModel, Base):
         Definition of the User class
     '''
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-
     if (storage_type == 'db'):
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
         reviews = relationship("Review", cascade="all, delete-orphan", backref="cities")
         places = relationship("Place", cascade="all, delete-orphan", backref="user")
     else:
