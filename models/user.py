a2f8dd2ab0f6cbdc40b9d3439e035ac05ed01820
Lisa Olson <olson.lisa94@gmail.com>
2018-07-26 02:47:50 +0000

    Adds imports

--- models/user.py
+++ models/user.py
@@ -4,9 +4,13 @@
 '''
 
 from os import getenv
-from models.base_model import BaseModel, Base
+import sqlalchemy
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, create_engine, DateTime
+from sqlalchemy import ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import ForeignKey
 
 storage_type = getenv('HBNB_TYPE_STORAGE')
 
