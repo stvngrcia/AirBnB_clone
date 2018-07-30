d9180e3fdbefb2c44712cc841f49519c97e5fdff
Kevin Yook <yook00627@gmail.com>
2018-07-30 09:43:44 -0700

    change it to pep8 compliant

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,11 +5,12 @@ from fabric.operations import local
 
 
 def do_pack():
+    """pack the file into tgz format"""
     try:
         local("mkdir -p versions")
         local("tar -cvzf versions/web_static_{}.tgz web_static/".format(
-            datetime.now().strftime("%Y%m%d%H%M%S"))
+            datetime.now().strftime("%Y%m%d%H%M%S")))
         return "versions/web_static_{}.tgz".format(
             datetime.now().strftime("%Y%m%d%H%M%S"))
     except Exception:
-              return None
+        return None
