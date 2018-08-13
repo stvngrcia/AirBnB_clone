deea4d65ca0560c100a08c9bdd1ac36f194c5777
Kevin Yook <yook00627@gmail.com>
2018-08-13 10:36:16 -0700

    starting web flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """display Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run()
