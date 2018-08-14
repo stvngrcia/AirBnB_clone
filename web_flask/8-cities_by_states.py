5fe6e2fc02450917746c9978844f54246ff96598
yook00627 <yook00627@gmail.com>
2018-08-13 17:22:23 -0700

    added web flask to print states and all cities

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage, classes
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown(excep):
+    """After each request you must remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/states_list')
+def states_list():
+    """display Hello HBNB!"""
+    states = storage.all(classes["State"]).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states')
+def cities_by_states():
+    """
+        Route /cities_by_states displays HTML listing each city in each state
+    """
+    states = storage.all(classes["State"]).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run()
